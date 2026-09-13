2a95d0cdb29abbd483407a67a85d521a6f5967a6
ammyskamble <ammyskamble@users.noreply.github.com>
2026-09-14 01:20:31 +0530

    Add sitemap.xml endpoint and sitemap link tags

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async () => {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://favvicon.com/sitemap-0.xml</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+  </sitemap>
+</sitemapindex>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  });
+};
