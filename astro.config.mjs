83f708d4a8bfb81c2ce56c9794987daf524ef8aa
ammyskamble <ammyskamble@users.noreply.github.com>
2026-09-14 01:28:37 +0530

    Update site domain to favvicon.pages.dev for sitemaps and metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://favvicon.com',
+  site: 'https://favvicon.pages.dev',
   trailingSlash: 'always',
   i18n: {
     defaultLocale: 'en',
--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,10 +1,11 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async () => {
+export const GET: APIRoute = async ({ site }) => {
+  const baseUrl = site ? site.href.replace(/\/$/, '') : 'https://favvicon.pages.dev';
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
-    <loc>https://favvicon.com/sitemap-0.xml</loc>
+    <loc>${baseUrl}/sitemap-0.xml</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
   </sitemap>
 </sitemapindex>`;
